6df201ea971508445a00eff08c5f0adadfba7a1a
Nemo <nemoshen94@gmail.com>
2024-01-02 20:54:16 +0800

    resolved: #1290

---
+++ leetcode/1290.二进制链表转整数.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=1290 lang=javascript
+ *
+ * [1290] 二进制链表转整数
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+  let number = '';
+  while (head) {
+    number += head.val
+    head = head.next
+  }
+  return parseInt(number, 2)
+};
+// @lc code=end
+
